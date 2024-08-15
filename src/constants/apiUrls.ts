c7eab1e3ce32c980eab19626101d87ac79140501
Rajalakshmy S <rajalakshmys27@gmail.com>
2024-08-15 22:17:38 +0530

    Updates

--- src/constants/apiUrls.ts
+++
@@ -1,4 +0,0 @@
-export const CSS_API_URL = "./data/cssBasicData.json";
-export const FLEX_BOX_API_URL = "./data/flexboxData.json";
-export const GRID_API_URL = "./data/gridData.json";
-export const ANIMATION_API_URL = "./data/animationData.json";
